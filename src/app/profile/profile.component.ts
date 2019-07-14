b271f6af3298d359d0fe2cfd22d94bf0213dc74e
MF Softworks <it@nygmarosebeauty.com>
2019-07-14 17:00:35 +0800

    [profile] implemented profile routing

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { profiles } from '../test-data/profiles';
 
 @Component({
     selector: 'app-profile',
     templateUrl: './profile.component.html',
     styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent { }
+export class ProfileComponent implements OnInit {
+    profiles = profiles;
+    profile: any;
+
+    constructor(
+        private route: ActivatedRoute,
+    ) { }
+
+    ngOnInit() {
+        this.route.paramMap.subscribe(params => {
+            for (const profile of profiles) {
+                if (profile.id.toString() === params.get('profileId')) { this.profile = profile; }
+            }
+        });
+    }
+}
