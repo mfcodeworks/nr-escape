5e971467862b3e812439b42e5aa2b254fff290b1
Arran Fletcher <arran.fletcher@linqcash.com>
2019-12-15 23:08:16 +0800

    [app] fixed mobile navigation

--- src/assets/js/boot.js
+++ src/assets/js/boot.js
@@ -2,10 +2,4 @@ window.onload = (event) => {
     console.log(
         !!(window.cordova || window.Cordova) ? 'Using Cordova' : 'Pure Angular'
     );
-    window.addEventListener = document.addEventListener = function () {
-        EventTarget.prototype.addEventListener.apply(this, arguments);
-    };
-    window.removeEventListener = document.removeEventListener = function () {
-        EventTarget.prototype.removeEventListener.apply(this, arguments);
-    };
 }
